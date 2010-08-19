c8474f7f7fc88b09fa06a8009151d5aa87b02808
deepsnow <doug.axtell@gmail.com>
2010-08-19 08:39:34 -0700

    removed some inefficiency

--- ProjectEuler/ProblemTwo/ProblemTwo.cpp
+++ ProjectEuler/ProblemTwo/ProblemTwo.cpp
@@ -6,50 +6,46 @@
 
 using namespace std;
 
-unsigned int genFib()
-{
-	static unsigned int a = 0;
-	static unsigned int b = 1;
-	
-	unsigned int result = a + b;
-	a = b;
-	b = result;
-
-	return result;
-}
+//unsigned int genFib()
+//{
+//	static unsigned int a = 0;
+//	static unsigned int b = 1;
+//	
+//	unsigned int result = a + b;
+//	a = b;
+//	b = result;
+//
+//	return result;
+//}
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 
-	//for(int i = 0; i < 10; i++)
-	//	cout << " " << genFib();
-	//cout << endl;
-
 	//long long sum = 0;
 	//unsigned int curr_num = genFib();
-	//for(int i = 0; i < 10; i++)
+	//while(curr_num < 4000000)
 	//{
-	//	if( (curr_num % 2) == 0 )
-	//	{
+	//	//if( (curr_num % 2) == 0 )
+	//	if( !(curr_num & 1) )
 	//		sum += static_cast<long long>(curr_num);
-	//		cout << "sum: " << sum << endl;
-	//	}
 	//	curr_num = genFib();
 	//}
+	//cout << sum << endl;
 
-	long long sum = 0;
-	unsigned int curr_num = genFib();
-	while(curr_num < 4000000)
+	int a, b, fib, sum;
+	a = fib = sum = 0;
+	b = 1;
+
+	do
 	{
-		//if( (curr_num % 2) == 0 )
-		if( !(curr_num & 1) )
-			sum += static_cast<long long>(curr_num);
-		curr_num = genFib();
-	}
-
+		fib = a + b;
+		a = b;
+		b = fib;
+		if( !(fib & 1) )
+			sum += fib;
+	} while (fib < 4000000);
 	cout << sum << endl;
 
 	return 0;
 }
-
