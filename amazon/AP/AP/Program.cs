bf760baff470afaa9d3ada8b86bd1bd0ca0d6d97
Doug <doug.axtell@gmail.com>
2015-06-01 15:22:06 -0400

    improved my solution to the amazon.com/hacherrank.com challenge I was given by amazon's recruiters

--- amazon/AP/AP/Program.cs
+++ amazon/AP/AP/Program.cs
@@ -30,61 +30,48 @@ namespace AP
         {
             //int N = ri.GetNumInputs();
             string apStr = ri.GetApString();
-            int prevValue = Int32.MaxValue;
-            List<ApEntry> deltaInfos = new List<ApEntry>();
-            int currPosition = 0;
             int result = Int32.MaxValue;
 
             List<string> progValues = apStr.Split(' ').ToList();
+            int valCount = progValues.Count;
 
-            foreach (var currValStr in progValues)
+            int A = GetIntVal(progValues[0]);
+            int B = GetIntVal(progValues[1]);
+            int C = GetIntVal(progValues[2]);
+            int D = GetIntVal(progValues[3]);
+
+            int deltaAB = B - A;
+            int deltaBC = C - B;
+            int deltaCD = D - C;
+
+            if ((deltaAB == deltaBC) && (deltaBC == deltaCD))
             {
-                int currValue = GetIntVal(currValStr);
-                if (prevValue != Int32.MaxValue)
+                int prevValue = D;
+                for (int i = 4; i < valCount; i++)
                 {
-                    int diff = currValue - prevValue;
-
-                    if (deltaInfos.Count < 3)
-                        deltaInfos.Add(new ApEntry() {delta = diff, position = currPosition});
-                    if (deltaInfos.Count == 3)
-                    {
-                        List<int> distinctDeltas = deltaInfos.Select(x => x.delta).Distinct().ToList();
-
-                        if (distinctDeltas.Count == 2)
-                        {
-                            result = LocateAndReturnMissingValue(deltaInfos, distinctDeltas, progValues);
-                            break;
-                        }
-                        else
-                        {
-                            //ditch youngest delta and add newest, unless it's different
-                        }
-                    }
+                    int currValue = GetIntVal(progValues[i]);
+                    if (currValue != (prevValue + deltaCD))
+                        result = prevValue + deltaCD;
+                    prevValue = currValue;
                 }
-
-                prevValue = currValue;
-                currPosition++;
+            }
+            else
+            {
+                if (deltaAB == deltaBC)
+                {
+                    result = C + deltaAB;
+                }
+                else if (deltaAB == deltaCD)
+                {
+                    result = C - deltaCD;
+                }
+                else
+                {
+                    result = B - deltaBC;
+                }
             }
 
             return result;
-        }
-
-        private int LocateAndReturnMissingValue(List<ApEntry> deltaInfos, List<int> distinctDeltas, List<string> progValues)
-        {
-            int indexOfAnomalousDelta = 0;
-            int indexOfNonAnomDelta = 1;
-
-            int candidateDistinctDelta = distinctDeltas[indexOfAnomalousDelta];
-            if (deltaInfos.Where(x => x.delta == candidateDistinctDelta).Count() == 2)
-            {
-                indexOfAnomalousDelta = 1;
-                indexOfNonAnomDelta = 0;
-            }
-            int anomylousDelta = distinctDeltas[indexOfAnomalousDelta];
-
-            int indexNearAnomalyInAp = deltaInfos.Single(x => x.delta.Equals(anomylousDelta)).position;
-
-            return GetIntVal(progValues[indexNearAnomalyInAp]) - distinctDeltas[indexOfNonAnomDelta];
         }
     }
 
@@ -110,6 +97,30 @@ namespace AP
             int answer = f.FindMissingNumber();
             Assert.AreEqual(answer, 7);
         }
+
+        [Test]
+        public void FindMissingNumber_SecondInput_AnomalyFound()
+        {
+            var f = new ApAnomalyFinder(new ReadInputsSecond());
+            int answer = f.FindMissingNumber();
+            Assert.AreEqual(answer, 3);
+        }
+
+        [Test]
+        public void FindMissingNumber_ThirdInput_AnomalyFound()
+        {
+            var f = new ApAnomalyFinder(new ReadInputsThird());
+            int answer = f.FindMissingNumber();
+            Assert.AreEqual(answer, 5);
+        }
+
+        [Test]
+        public void FindMissingNumber_FourthInput_AnomalyFound()
+        {
+            var f = new ApAnomalyFinder(new ReadInputsFourth());
+            int answer = f.FindMissingNumber();
+            Assert.AreEqual(answer, 20);
+        }
     }
 
     interface IReadInputs
@@ -132,6 +143,45 @@ namespace AP
         {
             //return Console.ReadLine(); // ditto: replace with mockable interface
             return "1 3 5 9 11";
+        }
+    }
+
+    class ReadInputsSecond : IReadInputs
+    {
+        public int GetNumInputs()
+        {
+            return 5;
+        }
+
+        public string GetApString()
+        {
+            return "1 5 7 9 11";
+        }
+    }
+
+    class ReadInputsThird : IReadInputs
+    {
+        public int GetNumInputs()
+        {
+            return 5;
+        }
+
+        public string GetApString()
+        {
+            return "1 3 7 9 11";
+        }
+    }
+
+    class ReadInputsFourth : IReadInputs
+    {
+        public int GetNumInputs()
+        {
+            return 8;
+        }
+
+        public string GetApString()
+        {
+            return "-16 -10 -4 2 8 14 26 32";
         }
     }
 }
